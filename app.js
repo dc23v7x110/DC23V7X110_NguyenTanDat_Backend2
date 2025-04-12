dfb142427f28fbb9b3cd556fae098e1fd613c56e
Nguyen Tan Dat <datdc23v7x110@dttx.ctu.edu.vn>
2025-04-12 23:22:32 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRoutes = require("./app/routes/contact.route.js");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRoutes);
 
 app.get("/",(req, res) => {
     res.json({ message: "Welcome to contact book application. " });
