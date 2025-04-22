f84034b947ebcb0f33cd8836eaac192af762f360
Nguyen Tan Dat <datdc23v7x110@dttx.ctu.edu.vn>
2025-04-22 21:37:20 +0700

    Revert "Cat dat cac handler truy xuat CSDL"
    This reverts commit 777ac812ce591722749e429cb33cc614a66aaf84.

--- app/config/index.js
+++ app/config/index.js
@@ -1,9 +1,6 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
-    },
-    db: {
-        uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/contactbook',
     }
 };
 
